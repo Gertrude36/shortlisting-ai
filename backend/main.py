cfcb17180698a2cf36b5dd2b203410493d437124
Gertrude36 <aishortlisting@gmail.com>
2026-04-20 18:11:47 +0200

    Fixed login bug and updated UI

--- backend/main.py
+++ backend/main.py
@@ -116,7 +116,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins     = ["http://localhost:5173", "http://localhost:3000"],
+    allow_origins     = ["https://shortlisting-ai.vercel.app/", "https://shortlisting-ai.onrender.com/"],
     allow_credentials = True,
     allow_methods     = ["*"],
     allow_headers     = ["*"],
