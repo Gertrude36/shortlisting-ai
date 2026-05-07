52ba960b2a6796365a8c6a058da55bde63f50041
Gertrude36 <aishortlisting@gmail.com>
2026-05-07 17:12:58 +0200

    fix: add frontend env file

--- backend/main.py
+++ backend/main.py
@@ -192,7 +192,6 @@ ALLOWED_ORIGINS = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
-    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
